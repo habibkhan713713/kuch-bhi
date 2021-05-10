5658a18ebafb37530557ab045d03dded4f90754e
Habibullah khan <habibkhan713713@gmail.com>
2021-05-11 01:34:27 +0500

    hogaya

---
+++ android/app/src/main/kotlin/com/example/pehliapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pehliapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(Pehliapp());
+  
+}
+
+
+
+
+
+
+
+
+class Pehliapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:Text("Habib Ullah Khan,habibkhan713713@gmail.com"),
+
+
+
+
+      
+      ),
+      
+    );
+  }
+}
